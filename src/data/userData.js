282eadde1b8a10abd7ee054f1725c6a8e0191c3d
ABDELHAMED MOHAMED <abdelhamedmohammedyehia@gmail.com>
2026-05-19 17:48:58 +0300

    Update userData.js

--- src/data/userData.js
+++ src/data/userData.js
@@ -1,22 +1,22 @@
-sofideverData = {
+export const userData = {
   logo: "/img/icons/sofi-icon.webp",
   name: "FOX",
   lastName: "Dev",
-  userName: "MexicoDev",
-  sloganHero1: "Javascript Developer Egypt",
+  userName: "FOXDev",
+  sloganHero1: "Javascript Developer in Egypt",
   profilePicture: "/img/raw.png",
   profilePicture2: "/img/raw.png",
   aboutPicture:
     "/img/shophi.png",
   aboutSkills: {
     intro:
-      "Skilled in modern web technologies, I create dynamic and user-centric digital solutions. \n\nMy expertise in both front-end and back-end development allows for the seamless integration of design and functionality, ensuring impactful and engaging applications.\n",
+      "Skilled in modern web technologies, I create dynamic and user-centric digital solutions. \n\nمؤرمبمببMy expertise in both front-end and back-end development allows for the seamless integration كصهلنلنببsign and functionality, ensuring impactful and engaging applications.\n",
     highlighted: "Tools",
     continuation:
-      "Leveraging advanced tools and platforms, I streamline both design and development processes. \n\n This approach ensures high-quality results, blending technical proficiency with creative design to enhance project outcomes.🚀",
+      "Leveraging advanced tools and platforms, I streamline both design and development فوكس الفجؤر. \nميييميي\n This approach ensures high-quality results, blending technical proficiency with creative design to enhance project outcomes.🚀",
   },
   terminalInfo:
-    "ls skills\n Javascript, React, Astro, NextJs Node.js, Express, Sass, Tailwind, Scss, Bootstrap, MySQL, Wordpress, CSS, Vue.js, Svelte\n\nls tools\n- figma, vs code, vercel\nbash, github, git, photoshop\n illustrator, ",
+    "ls skills\nيينيرمرببمب Javascript, React, Astro, NextJs Node.js, Express, Sass, Tailwind, Scss, Bootstrap, MySQL, Wordpress, CSS, Vue.js, Svelte\n\nls tools\n- figma, vs code, vercel\nbash, github, git, photoshop\n illustrator, ",
   cv: "/docs/CV_AngelaSofía_Osorio_2025.pdf",
-  email: "Medohackerz@Fox.top",
+  email: "contact@sofidev.top",
 };
