0f299d885bfd7d4d986e794b6e146aece7ee1c37
ABDELHAMED MOHAMED <abdelhamedmohammedyehia@gmail.com>
2026-05-19 22:48:20 +0300

    Update socialIconsData.js

--- src/data/socialIconsData.js
+++ src/data/socialIconsData.js
@@ -2,16 +2,16 @@ export const socialIconsData = [
   {
     icon: "fa:github-alt",
     name: "GitHub",
-    link: "https://github.com/SofiDevO",
+    link: "https://github.com/Medohackers",
   },
   {
     icon: "simple-icons:linkedin",
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/sofidev/",
+    link: "https://www.linkedin.com/in/abdelhamed-mohamad-1295592b2/",
   },
   {
     icon: "entypo-social:youtube-with-circle",
     name: "YouTube",
-    link: "https://www.youtube.com/@SofiDev",
+    link: "https://www.youtube.com/@X_MR_FOX_X",
   },
 ];
