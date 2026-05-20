ed0c2ad9d663e9f761b7af4cd966cfd5717806c3
ABDELHAMED MOHAMED <abdelhamedmohammedyehia@gmail.com>
2026-05-21 01:37:51 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://itssofi.dev",
+  site: "https://portfolio-sofi2-0-eight.vercel.app/",
   env: {
     schema: {
       LOCAL_URL: envField.string({
