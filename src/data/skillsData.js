322c8104f38ee16257e8a103c5e109d233df930b
ABDELHAMED MOHAMED <abdelhamedmohammedyehia@gmail.com>
2026-05-20 17:16:21 +0300

    Update skillsData.js

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -7,7 +7,7 @@ export const skillsData = [
   { name: "Vue.js", icon: "skill-icons:vuejs-dark" },
   { name: "CSS", icon: "vscode-icons:file-type-css" },
   { name: "git", icon: "skill-icons:git" },
-  { name: "ubuntu", icon: "logos:ubuntu" },
+  { name: "terminal", icon: "logos:terminal" },
   { name: "Tailwind", icon: "skill-icons:tailwindcss-dark" },
   { name: "MySQL", icon: "skill-icons:mysql-dark" },
   { name: "github", icon: "skill-icons:github-light" },
